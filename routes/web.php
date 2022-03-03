1b72baa8a74547febea726dac0917b4e62f35a65
MikeG <groshevm@mail.ru>
2022-03-03 11:30:56 +0300

    lesson_1

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,48 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
+Route::get('home', function (){
+   return '
+   <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Home</title>
+    </head>
+    <body>
+        <h1>Welcome Laravel Lessons</h1>
+    </body>
+    </html>
+   ';
+});
+Route::get('post', function (){
+    return '
+       <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Posts</title>
+    </head>
+    <body>
+        <h1>Страница постов</h1>
+    </body>
+    </html>
+    ';
+});
+Route::get('description', function (){
+    return '
+       <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Description</title>
+    </head>
+    <body>
+        <h1>Страница описания</h1>
+    </body>
+    </html>
+    ';
+});
